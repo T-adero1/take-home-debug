edaabfaf05a283c715319307709ce751774b3611
Tobi Aderounmu <tobi.aderounmu@magic.link>
2025-11-03 14:12:28 -0600

    Update magic.js

--- src/lib/magic.js
+++ src/lib/magic.js
@@ -6,7 +6,7 @@ export const magic = new Magic(process.env.REACT_APP_MAGIC_API_KEY, {
   extensions: [
     new OAuthExtension(),
     new SolanaExtension({
-      rpcUrl: "dal17.nodes.rpcpool.com", // Bitcoin mainnet RPC// or 'testnet' for testing
+      rpcUrl: "dal17.nodes.rpcpool.com",
     }),
   ],
 });
